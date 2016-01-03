beb6cab97aa8d5487187cc1b7ea3ec12029a6ff9
Davis <davisclaib@gmail.com>
2016-01-03 12:39:21 -0500

    Update gen.sh

--- gen/gen.sh
+++ gen/gen.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-
-rm -r love-api
+cd "$(dirname "$0")"
+set -e
+rm -rf love-api
 git clone https://github.com/rm-code/love-api
 love .> out.txt
-rm -r love-api
+rm -rf love-api
+mv out.txt ../syntax/lua.vim
