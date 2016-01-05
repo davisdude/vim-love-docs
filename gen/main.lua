eb8ab24390abbc738e88d99523df8ad2e9185b44
Davis Claiborne <davisclaib@gmail.com>
2016-01-05 17:06:25 -0500

    Speed and highlighting improvements

--- gen/main.lua
+++ gen/main.lua
@@ -2,31 +2,71 @@ local api = require 'love-api.love_api'
 
 local oldprint = print
 local function print( group, str, contained )
-	oldprint( 'syntax match ' .. group .. ' ' .. '"' .. str:gsub( '[%.:]', '\\%1' ) .. '" ' .. ( contained or '' ) )
+	oldprint( 'syntax match ' .. group .. ' ' .. '"' .. str:gsub( '[%.:]', '\\%1' ) .. '"' .. ( contained or '' ) )
 end
 
-oldprint( 'syntax match lovepunctuation "[\\:\\.]" nextgroup=lovetype' )
+
+oldprint( [[if exists( "b:love_syntax" )
+	finish
+endif]] )
+oldprint( 'let b:love_syntax = 1' )
+
+local originalfuncstr = 'syntax match lovefunction "\\(^\\|\\s\\)love\\.\\('
+local funcstr = originalfuncstr
+
+local originaltypestr = 'syntax match lovetype "[\\:\\.]\\('
+local typestr = originaltypestr
+
+local originalcallbackstr = 'syntax match lovefunction "\\(^\\|\\s\\)love\\.\\('
+local callbackstr = originalcallbackstr
+
 local function extractData( tab, index )
 	for i, v in pairs( tab ) do
 		if i == 'functions' or i == 'callbacks' then
+			if tab.name and ( tab.name or '' ):sub( 1, 1 ):match( '%l' ) then funcstr = funcstr .. tab.name .. '\\.\\(' end
+			local func = false
+			local typ = false
+			local callback = false
 			for _, vv in pairs( v ) do
 				if tab.name then
 					if tab.name:sub( 1, 1 ):match( '%l' ) then
-						print( 'lovefunction', 'love.' .. tab.name .. '.' .. vv.name )
+						func = true
+						funcstr = funcstr .. vv.name .. '\\|'
 					else -- types
-						print( 'lovetype', vv.name, 'contained' )
+						typ = true
+						typestr = typestr .. vv.name .. '\\|'
 					end
 				else
-					print( 'lovefunction', 'love.' .. vv.name )
+					callback = true
+					callbackstr = callbackstr .. vv.name .. '\\|'
 				end
+			end
+			if func then 
+				-- We don't want to be able to have underscores after the word
+				funcstr = funcstr:sub( 1, -3 ) .. '\\)\\)\\_[^_a-zA-Z]"me=e-1'
+				oldprint( funcstr )
+				funcstr = originalfuncstr
+			end
+			if typ then
+				-- We don't want to be able to have underscores after the word or highlight the . or :
+				typestr = typestr:sub( 1, -3 ) .. '\\)\\_[^_a-zA-Z]"ms=s+1,me=e-1'
+				oldprint( typestr )
+				typestr = originaltypestr
+			end
+			if callback then 
+				-- We don't want to be able to have underscores after the word
+				callbackstr = callbackstr:sub( 1, -3 ) .. '\\)\\_[^_a-zA-Z]"me=e-1'
+				oldprint( callbackstr )
+				callbackstr = originalcallbackstr
 			end
 		end
 		if type( v ) == 'table' then extractData( v ) end
 	end
 end
 extractData( api )
+
+oldprint( 'let b:current_syntax = "lua"' )
 oldprint( 'highlight lovefunction guifg=#ff60e2' )
 oldprint( 'highlight lovetype guifg=#ff60e2' )
-oldprint( 'highlight lovepunctuation NONE' )
 
 love.event.quit()
