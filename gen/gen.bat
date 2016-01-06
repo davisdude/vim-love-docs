4994a13c9291262a6788d127f9cef8b5f6e6fa6e
Davis <davisclaib@gmail.com>
2016-01-06 07:53:08 -0500

    Changed from absolute path

--- gen/gen.bat
+++ gen/gen.bat
@@ -5,7 +5,7 @@ cd %~dp0
 rd /q /s love-api
 git clone https://github.com/rm-code/love-api
 
-"C:\Program Files\LOVE\0.10.0\love" . >out.txt
+love . >out.txt
 
 if not exist ..\syntax mkdir ..\syntax
 move out.txt ..\syntax\lua.vim
